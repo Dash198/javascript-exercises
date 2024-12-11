de7725363af5cf90f5cc2f80bf840e08e3c54f5f
Dash198 <92976625+Dash198@users.noreply.github.com>
2024-12-11 19:38:22 +0530

    basics done

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,11 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(temp) {
+  let nT=5/9*(temp-32);
+  return Math.round(nT*10)/10;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(temp) {
+  let nT=9/5*temp + 32;
+  return Math.round(nT*10)/10;
 };
 
 // Do not edit below this line
